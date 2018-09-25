4ea5697051ba354330b131d08580873e591ff1e5
维啸 廖 <1336710420@qq.com>
2018-09-25 21:19:49 +0800

    第三次作业

---
+++ Homework3/program1/Figure.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//图像的处理
+namespace FigureClass
+{
+    public interface Figure
+    {
+        double getArea(double param1,double param2);//用两个参数计算图形面积   
+    }
+    //三角形类
+    public class Triangle : Figure
+    {
+        public double getArea(double ButtonLength, double Height)
+        {
+            return 0.5 * ButtonLength * Height;
+        }
+    }
+    //椭圆形类
+    public class Circle : Figure
+    {
+        public double getArea(double Radius1,double Radius2)
+        {
+            return 3.14 * Radius1 * Radius2;
+        }
+    }
+    //矩形类
+    public class Rectangle : Figure
+    {
+     
+        public double getArea(double Length, double Width)
+        {
+            return Length * Width;
+        }
+    }
+    //简单工厂类 
+    public class FigureFactory
+    {
+        public Figure CreateFigure(string FigureName)
+        {
+            switch (FigureName)
+            {
+                case "三角形":
+                    return new Triangle();
+                case "椭圆形":
+                    return new Circle();
+                case "矩形":
+                    return new Rectangle();
+
+                default:
+                    throw new Exception("工厂中没有该图形");
+            }
+        }
+    }
+
+}
---
+++ Homework3/program1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FigureClass;
+
+namespace program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Area = 0;
+            FigureFactory Factory = new FigureFactory();
+
+            //从工厂中创建三角形，并计算面积
+            Figure A = Factory.CreateFigure("三角形"); 
+            Area = A.getArea(10,100);
+            Console.WriteLine("创建三角形，其面积为：" + Area);
+
+            //从工厂中创建圆形，并计算面积
+            Figure B = Factory.CreateFigure("椭圆形"); 
+            Area = B.getArea(10, 10);
+            Console.WriteLine("创建圆形，其面积为：" + Area);
+
+            //从工厂中创建正方形，并计算面积
+            Figure C = Factory.CreateFigure("矩形"); 
+            Area = C.getArea(10, 10);
+            Console.WriteLine("创建正方形，其面积为：" + Area);
+
+            //从工厂中创建长方形，并计算面积
+            Figure D = Factory.CreateFigure("矩形"); 
+            Area = D.getArea(10, 30);
+            Console.WriteLine("创建长方形，其面积为：" + Area);
+            Console.ReadKey();
+        }
+    }
+}
