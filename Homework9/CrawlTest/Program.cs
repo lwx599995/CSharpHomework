8b61eebbb9d092bf687844ec8b6c7083f622c7e9
维啸 廖 <1336710420@qq.com>
2018-11-22 21:02:53 +0800

    第九次作业

---
+++ Homework9/CrawlTest/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CrawlTest
+{
+    public class Crawler
+    {
+        private Hashtable urls = new Hashtable();
+        private int count = 0;
+        static void Main(string[] args)
+        {
+            Crawler myClawer = new Crawler();
+            string[] starturls = {"https://www.baidu.com/",
+                                    "https://hao.360.cn/" };
+            foreach (string url in starturls)
+                myClawer.urls.Add(url, false);   //加入初始界面
+            new Thread(myClawer.Crawl).Start();   //开始爬行
+        }
+        private void Crawl()
+        {
+            Console.WriteLine("开始爬行了......");
+            List<string> list = new List<string>();
+            foreach (string url in urls.Keys)
+            {
+                if ((bool)list.Contains(url)) continue;    //已经添加过的不再添加
+                list.Add(url);
+            }
+            Parallel.ForEach(list, (string url) =>         //并行处理
+            {
+                string current = url;
+                Console.WriteLine("爬行" + current + "页面！");
+                string html = DownLoad(current);   //下载
+                urls[current] = true;
+                Parse(html);
+                count++;
+            });
+            Console.WriteLine("爬行结束");
+        }
+        public string DownLoad(string url)
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                webClient.Encoding = Encoding.UTF8;
+                string html = webClient.DownloadString(url);
+                string filename = count.ToString()+".html";
+                File.WriteAllText(filename, html, Encoding.UTF8);
+                return html;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return "";
+            }
+        }
+        public void Parse(string html)
+        {
+            string strRef = @"(href|HREF) [] * = [] * [""'][^""'#>]+[""']";
+
+            MatchCollection matches = new Regex(strRef).Matches(html);
+            foreach (Match match in matches)
+            {
+                strRef = match.Value.Substring(match.Value.IndexOf('=') + 1).
+                    Trim('"', '\"', '#', ' ', '>');
+                if (strRef.Length == 0) continue;
+                if (urls[strRef] == null) urls[strRef] = false;
+            }
+        }
+    }
+}
---
+++ Homework9/MysqlTest/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using ordertest;
+
+namespace MysqlTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer(1, "张三", 15270390336);
+            Customer customer2 = new Customer(2, "李四", 13634097159);
+            Goods apple = new Goods(3, "apple", 5.6);
+            Goods egg = new Goods(2, "egg", 4.9);
+            Goods milk = new Goods(1, "milk", 69.9);
+            OrderDetail orderDetails1 = new OrderDetail(1, apple, 8);
+            OrderDetail orderDetails2 = new OrderDetail(2, egg, 2);
+            OrderDetail orderDetails3 = new OrderDetail(3, milk, 1);
+            Order order1 = new Order(20181111111, customer1);
+            order1.AddDetails(orderDetails1);
+            order1.AddDetails(orderDetails2);
+            order1.AddDetails(orderDetails3);
+            string connectStr = "server=localhost;port=3306;database=od;user=root;password=lwx.19993515;";
+            //并没有建立数据库连接
+            MySqlConnection conn = new MySqlConnection(connectStr);
+            try
+            {   //建立连接，打开数据库
+                conn.Open();
+                string sqlstr = $"insert into orders(OrderID,Customer,Detials) values(" +
+                    $"'{order1.Id.ToString()}'," +
+                    $"'{order1.Customer.ToString()}'," +
+                    $"{order1.Details.ToString()})";   
+                MySqlCommand cmd = new MySqlCommand(sqlstr, conn);
+
+                int result = cmd.ExecuteNonQuery();   //返回值为执行后数据库中受影响的数据行数
+                Console.WriteLine("执行成功，影响了{0}行数据", result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                conn.Close();   //关闭连接
+            }
+            Console.ReadKey();
+        }
+
+
+        
+    
+}
